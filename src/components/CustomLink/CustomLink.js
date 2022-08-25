34c8568f9489fd723108a3c90fce8a3142078884
JooJo <sjak0101@gmail.com>
2022-08-25 20:47:17 +0100

    added some Link routing

--- src/components/CustomLink/CustomLink.js
+++ src/components/CustomLink/CustomLink.js
@@ -8,7 +8,7 @@ function CustomLink({ children, to, ...props }) {
     return (
       <div>
         <Link
-          style={{ textDecoration: match ? "underline" : "none" }}
+          style={{ color:match? 'red': 'black', textDecoration: match ? "underline" : "none" }}
           to={to}
           {...props}
         >
